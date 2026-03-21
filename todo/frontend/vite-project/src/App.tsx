4fea4881f4b1a5a5e8cd915307ac73334a99f5d8
barelysomethin <abc@example.com>
2026-03-21 17:27:32 +0530

    init the frontend

---
+++ todo/frontend/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+function App () {
+
+    return (
+        <div className='container'>
+            <button className="button">getallnotes</button>
+            <button className="button">createanote</button>
+            <button className="button">getanotebyID</button>
+            <button className="button">deleteanotebyID</button>
+             <button className="button">updateanotebyID</button>
+        </div>
+    )
+}
+export default App;
